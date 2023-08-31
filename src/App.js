2680748b86547c363fc63a0b0ba4d1f6bac5e536
Andrey Emelianov <621077@mail.ru>
2023-08-31 18:45:10 +0300

    подключил вспомогательную библиотеку для css macro-css, базово сверстал хедер

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 function App() {
 	return (
-		<div className="wrapper">
-			<header>
-				<div className="headerLeft">
+		<div className="wrapper clear">
+			<header className="d-flex justify-between align-center p-40">
+				<div className="d-flex align-center">
 					<img width={40} height={40} src="/img/logo.png" alt="Логотип магазина" />
-					<div className="headerInfo">
-						<h3>Drem Sneakers</h3>
+					<div>
+						<h3 className="text-uppercase">Drem Sneakers</h3>
 						<p>Магазин модных кроссовок</p>
 					</div>
 				</div>
 
-				<ul className="headerRight">
-					<li>
+				<ul className="d-flex">
+					<li className="mr-30">
 						<img width={18} height={18} src="/img/cart.svg" alt="иконка корзины" />
 						<span>1205 руб.</span>
 					</li>
@@ -21,7 +21,7 @@ function App() {
 				</ul>
 			</header>
 
-			<div className="content">
+			<div className="content p-40">
 				<h1>Все кроссовки</h1>
 			</div>
 		</div>
