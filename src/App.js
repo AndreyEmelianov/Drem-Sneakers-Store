23e0f300526fb9587783dd8a07aff24321576404
Andrey Emelianov <621077@mail.ru>
2023-08-31 18:26:54 +0300

    добавил изображения и иконки

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
 		<div className="wrapper">
 			<header>
 				<div className="headerLeft">
-					<svg />
+					<img width={40} height={40} src="/img/logo.png" alt="Логотип магазина" />
 					<div className="headerInfo">
 						<h3>Drem Sneakers</h3>
 						<p>Магазин модных кроссовок</p>
@@ -12,14 +12,18 @@ function App() {
 
 				<ul className="headerRight">
 					<li>
-						<svg />
+						<img width={18} height={18} src="/img/cart.svg" alt="иконка корзины" />
 						<span>1205 руб.</span>
 					</li>
 					<li>
-						<svg />
+						<img width={18} height={18} src="/img/user.svg" alt="иконка профиля" />
 					</li>
 				</ul>
 			</header>
+
+			<div className="content">
+				<h1>Все кроссовки</h1>
+			</div>
 		</div>
 	);
 }
