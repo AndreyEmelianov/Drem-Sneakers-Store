bdd346c434f71e384be9ef561a676e1a57f40aa1
Andrey Emelianov <621077@mail.ru>
2023-08-31 19:12:57 +0300

    сделал базовую вёрстку карточки товара

--- src/App.js
+++ src/App.js
@@ -22,7 +22,65 @@ function App() {
 			</header>
 
 			<div className="content p-40">
-				<h1>Все кроссовки</h1>
+				<h1 className="mb-40">Все кроссовки</h1>
+
+				<div className="d-flex">
+					<div className="card">
+						<img width={133} height={112} src="/img/sneakers/1.jpg" alt="кроссовки" />
+						<h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+						<div className="d-flex justify-between align-center">
+							<div className="d-flex flex-column">
+								<span>Цена:</span>
+								<b>12 999 руб.</b>
+							</div>
+							<button className="button">
+								<img width={11} height={11} src="/img/plus.svg" alt="кнопка добавить" />
+							</button>
+						</div>
+					</div>
+
+					<div className="card">
+						<img width={133} height={112} src="/img/sneakers/2.jpg" alt="кроссовки" />
+						<h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+						<div className="d-flex justify-between align-center">
+							<div className="d-flex flex-column">
+								<span>Цена:</span>
+								<b>12 999 руб.</b>
+							</div>
+							<button className="button">
+								<img width={11} height={11} src="/img/plus.svg" alt="кнопка добавить" />
+							</button>
+						</div>
+					</div>
+
+					<div className="card">
+						<img width={133} height={112} src="/img/sneakers/3.jpg" alt="кроссовки" />
+						<h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+						<div className="d-flex justify-between align-center">
+							<div className="d-flex flex-column">
+								<span>Цена:</span>
+								<b>12 999 руб.</b>
+							</div>
+							<button className="button">
+								<img width={11} height={11} src="/img/plus.svg" alt="кнопка добавить" />
+							</button>
+						</div>
+					</div>
+
+					<div className="card">
+						<img width={133} height={112} src="/img/sneakers/4.jpg" alt="кроссовки" />
+						<h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+						<div className="d-flex justify-between align-center">
+							<div className="d-flex flex-column">
+								<span>Цена:</span>
+								<b>12 999 руб.</b>
+							</div>
+							<button className="button">
+								<img width={11} height={11} src="/img/plus.svg" alt="кнопка добавить" />
+							</button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
