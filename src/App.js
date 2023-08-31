277632c6fc080fb44ac6efe3da65712da6cdd07c
Andrey Emelianov <621077@mail.ru>
2023-08-31 20:51:42 +0300

    сделал внизу корзины кнопку заказа , информацию о цене и налоге

--- src/App.js
+++ src/App.js
@@ -3,32 +3,54 @@ function App() {
 		<div className="wrapper clear">
 			<div className="overlay">
 				<div className="drawer">
-					<h2 className="mb-30">Корзина</h2>
+					<h2 className="d-flex justify-between mb-30 ">
+						Корзина{' '}
+						<img className="cu-p removeBtn" src="/img/btn-remove.svg" alt="кнопка удалить" />
+					</h2>
 
-					<div className="cartItem d-flex align-center mb-20">
-						<div
-							style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
-							className="cartItemImg"
-						></div>
+					<div className="items">
+						<div className="cartItem d-flex align-center mb-20">
+							<div
+								style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+								className="cartItemImg"
+							></div>
 
-						<div className="mr-20 flex">
-							<p className="mb-5">Мужские кроссовки Nike Air Max 270</p>
-							<b>12 999 руб.</b>
+							<div className="mr-20 flex">
+								<p className="mb-5">Мужские кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<img className="removeBtn" src="/img/btn-remove.svg" alt="кнопка удалить" />
 						</div>
-						<img className="removeBtn" src="/img/btn-remove.svg" alt="кнопка удалить" />
-					</div>
 
-					<div className="cartItem d-flex align-center mb-20">
-						<div
-							style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
-							className="cartItemImg"
-						></div>
+						<div className="cartItem d-flex align-center mb-20">
+							<div
+								style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
+								className="cartItemImg"
+							></div>
 
-						<div className="mr-20 flex">
-							<p className="mb-5">Мужские кроссовки Nike Air Max 270</p>
-							<b>12 999 руб.</b>
+							<div className="mr-20 flex">
+								<p className="mb-5">Мужские кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<img className="removeBtn" src="/img/btn-remove.svg" alt="кнопка удалить" />
 						</div>
-						<img className="removeBtn" src="/img/btn-remove.svg" alt="кнопка удалить" />
+						<div className="cartTotalBlock">
+							<ul>
+								<li>
+									<span>Итого:</span>
+									<div></div>
+									<b>21 500 руб. </b>
+								</li>
+								<li>
+									<span>Налог 5%:</span>
+									<div></div>
+									<b>1075 руб. </b>
+								</li>
+							</ul>
+							<button className="greenButton">
+								Оформить заказ <img src="/img/arrow.svg" alt="иконка стрелочки" />
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
